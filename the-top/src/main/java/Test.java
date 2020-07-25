08a3ef6fcf8b0564ac7e478620c0e0475c4e4d94
jiawuyinzi <jiawuyinzi@163.com>
2020-07-25 10:47:05 +0800

    上传

--- the-top/src/main/java/Test.java
+++ the-top/src/main/java/Test.java
@@ -1,7 +1,12 @@
+import net.sf.json.JSONObject;
+
 public class Test {
 
     public static void main(String[] args) {
         JSONObject jsonObject = new JSONObject();
+        jsonObject.put("qq", "dsds");
+
+        System.out.println(jsonObject.get("qq"));
     }
 
 }
